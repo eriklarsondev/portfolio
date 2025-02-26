94da1337dc54e2daac89ac96668578fef435d5e3
Erik Larson <eriklarsonco@gmail.com>
2025-02-26 17:28:02 -0500

    fixed about page layout

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,7 +3,7 @@
     <PageHero slug="about-me" />
 
     <div class="container max-w-[1260px]">
-      <div class="flex lg:flex-row flex-col gap-20">
+      <div class="flex lg:flex-row flex-col lg:gap-20 gap-10">
         <div class="lg:w-80 w-full"></div>
 
         <div class="flex-1">
