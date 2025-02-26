72e0f1832eb53189bd0e60e359fd1c9f481cb049
Erik Larson <eriklarsonco@gmail.com>
2025-02-26 17:27:32 -0500

    added content to about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,6 +2,14 @@
   <div>
     <PageHero slug="about-me" />
 
-    <div class="container max-w-[1260px]"></div>
+    <div class="container max-w-[1260px]">
+      <div class="flex lg:flex-row flex-col gap-20">
+        <div class="lg:w-80 w-full"></div>
+
+        <div class="flex-1">
+          <Content slug="about-me" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
