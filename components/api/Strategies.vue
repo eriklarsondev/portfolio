95e102f4a3db5cd862d08b9f1a78d59522a73fa3
Erik Larson <eriklarsonco@gmail.com>
2025-03-12 08:48:23 -0400

    fixed mobile layout for strategies component

--- components/api/Strategies.vue
+++ components/api/Strategies.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-10 gap-y-20" v-if="data && data.length">
+    <div
+      class="grid xl:grid-cols-3 grid-cols-1 xl:gap-x-10 xl:gap-y-20 gap-y-10 xl:max-w-full max-w-3xl mx-auto"
+      v-if="data && data.length">
       <div
         v-for="(item, index) in data"
         :key="index"
-        class="text-center"
+        class="xl:text-center"
         data-aos="fade-right"
         :data-aos-delay="(index + 1) * 150">
         <em class="font-serif text-8xl text-accent">0{{ item.order }}</em>
 
-        <h2 class="mt-1 mb-0 text-3xl">{{ item.name }}</h2>
-        <span
-          class="strategy-content inline-block relative mt-2 ml-3 text-left leading-6 text-sm"
-          v-html="item.content"
-          v-if="item.content"></span>
+        <h2 class="mt-1 mb-0 xl:text-3xl md:text-4xl text-3xl">{{ item.name }}</h2>
+        <div class="inline-block mt-2 ml-3 text-left" v-if="item.content">
+          <span
+            class="strategy-content relative xl:leading-7 md:leading-10 leading-9 font-serif xl:text-base md:text-3xl text-2xl"
+            v-html="item.content"></span>
+        </div>
       </div>
     </div>
 
