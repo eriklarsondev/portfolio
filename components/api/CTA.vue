4258725f9a4c6c72c7efb33e5772a45a461e83e1
Erik Larson <eriklarsonco@gmail.com>
2025-03-16 19:50:25 -0400

    fixed cta component layout

--- components/api/CTA.vue
+++ components/api/CTA.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <div
-      class="relative sm:h-[45vw] md:min-h-[600px] sm:min-h-[500px] min-h-[unset] max-h-[800px] md:my-32 my-20 bg-zinc-900"
+      class="relative lg:h-[45vw] lg:min-h-[600px] min-h-[unset] md:max-h-[800px] md:my-32 my-20 bg-zinc-900"
       v-if="data">
       <div
-        class="absolute top-0 left-0 w-full h-full md:bg-fixed bg-cover bg-left-top grayscale"
+        class="absolute top-0 left-0 w-full h-full lg:bg-fixed bg-cover bg-left-top grayscale"
         :style="{ backgroundImage: `url(${data.image.url})` }"
         v-if="data.image"></div>
 
-      <div class="flex justify-center sm:items-end items-center sm:absolute w-full h-full top-0 left-0 bg-black/20">
+      <div class="absolute top-0 left-0 w-full h-full lg:bg-black/30 bg-black/50" v-if="data.image"></div>
+
+      <div class="flex justify-center md:items-end items-center lg:absolute w-full h-full top-0 left-0">
         <Container class="relative py-20 sm:text-center">
-          <Heading :cta="true" class="[&]:m-0 [&]:text-white" data-aos="fade-down" v-if="data.heading">
+          <Heading :cta="true" class="[&]:m-0 [&]:text-white/50" data-aos="fade-down" v-if="data.heading">
             <div v-html="data.heading"></div>
           </Heading>
 
