530bbdc40a1e0fb96d0f14fc332bdf5dd5799ae7
Erik Larson <eriklarsonco@gmail.com>
2025-03-16 18:33:58 -0400

    added error if cta is not found

--- components/api/CTA.vue
+++ components/api/CTA.vue
@@ -1,18 +1,23 @@
 <template>
-  <div
-    class="relative sm:h-[45vw] md:min-h-[600px] sm:min-h-[500px] min-h-[unset] max-h-[800px] md:my-32 my-20 bg-zinc-900">
+  <div>
     <div
-      class="absolute top-0 left-0 w-full h-full md:bg-fixed bg-cover bg-left-top grayscale"
-      :style="{ backgroundImage: `url(${data.image.url})` }"
-      v-if="data.image"></div>
+      class="relative sm:h-[45vw] md:min-h-[600px] sm:min-h-[500px] min-h-[unset] max-h-[800px] md:my-32 my-20 bg-zinc-900"
+      v-if="data">
+      <div
+        class="absolute top-0 left-0 w-full h-full md:bg-fixed bg-cover bg-left-top grayscale"
+        :style="{ backgroundImage: `url(${data.image.url})` }"
+        v-if="data.image"></div>
 
-    <div class="flex justify-center sm:items-end items-center sm:absolute w-full h-full top-0 left-0 bg-black/20">
-      <Container class="relative py-20 sm:text-center">
-        <Heading :cta="true" class="[&]:m-0" data-aos="fade-down">
-          <div v-html="data.heading"></div>
-        </Heading>
-      </Container>
+      <div class="flex justify-center sm:items-end items-center sm:absolute w-full h-full top-0 left-0 bg-black/20">
+        <Container class="relative py-20 sm:text-center">
+          <Heading :cta="true" class="[&]:m-0" data-aos="fade-down">
+            <div v-html="data.heading"></div>
+          </Heading>
+        </Container>
+      </div>
     </div>
+
+    <Error v-if="!data">{{ props.slug }} was not found</Error>
   </div>
 </template>
 
