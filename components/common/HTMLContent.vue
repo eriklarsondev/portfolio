8d41c27fa50fe35fe581a2c74c287706d027d78c
Erik Larson <eriklarsonco@gmail.com>
2025-04-08 03:03:34 -0400

    removed text justification from small screens

--- components/common/HTMLContent.vue
+++ components/common/HTMLContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['leading-9 font-thin text-[17.5px] text-zinc-300', { 'text-justify': props.justify }]">
+  <div :class="['leading-9 font-thin text-[17.5px] text-zinc-300', { 'lg:text-justify': props.justify }]">
     <div v-html="props.html"></div>
   </div>
 </template>
